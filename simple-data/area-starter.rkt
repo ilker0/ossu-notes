b33ecb316afb7874237252f452203a6505bbdaac
ilker0 <ilker46580@gmail.com>
2023-11-26 15:34:37 +0300

    simple-data: area-starter

---
+++ simple-data/area-starter.rkt
@@ -0,0 +1,26 @@
+
+;; area-starter.rkt
+
+; 
+; PROBLEM:
+; 
+; DESIGN a function called area that consumes the length of one side 
+; of a square and produces the area of the square.
+; 
+; Remember, when we say DESIGN, we mean follow the recipe.
+; 
+; Leave behind commented out versions of the stub and template.
+; 
+
+
+;; Number -> Number
+;; given length of one of square, produce the area of the square
+
+(check-expect (area 3) 9)
+(check-expect (area 3.2) (* 3.2 3.2))
+
+; (define (area s) 0 ) ;stub
+
+(define (area s)
+  (* s s))
+

7d670d97e5ba6a93cdf3507fcd2b86bfa6438155
ilker0 <ilker46580@gmail.com>
2023-11-26 15:48:33 +0300

    simple-data: image-area-starter

---
+++ simple-data/image-area-starter.rkt
@@ -0,0 +1,19 @@
+
+;; image-area-starter.rkt
+
+; 
+; PROBLEM:
+; 
+; DESIGN a function called image-area that consumes an image and produces the 
+; area of that image. For the area it is sufficient to just multiple the image's 
+; width by its height.  Follow the HtDF recipe and leave behind commented 
+; out versions of the stub and template.
+; 
+
+;; Image -> Number
+;; produce image's width * height (area)
+
+(check-expect (image-area (rectangle 2 3 "solid" "red")) (* 2 3))
+
+(define (image-area img)
+  (* (image-width img) (image-height img )))
